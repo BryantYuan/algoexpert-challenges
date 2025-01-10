3ca9348afce4e629bbce78a3217d07361b42fbd9
Jia Yie Yuan <bryanty1130@gmail.com>
2025-01-10 13:31:17 +1300

    Do oneEdit challenge.

---
+++ strings/medium/one_edit/program.py
@@ -0,0 +1,43 @@
+def oneEdit(stingOne: str, stingTwo: str) -> bool:
+
+    length1 = len(stingOne)
+    length2 = len(stingTwo)
+
+    if abs(length1 - length2) > 1:
+        return False
+
+
+    index1 = 0
+    index2 = 0
+    diff_count = 0
+
+    smallerLength = min(length1, length2)
+
+
+    while index1 < length1:
+
+        if stingOne[index1] != stingTwo[index2]:
+            diff_count += 1
+
+            if length1 == length2:
+
+                index1 += 1
+                index2 += 1
+
+                continue
+
+            if smallerLength == length1:
+                index2 += 1
+
+            else:
+                index1 += 1
+
+        else:
+
+            index1 += 1
+            index2 += 1
+
+        if diff_count > 1:
+            return False
+
+    return True
